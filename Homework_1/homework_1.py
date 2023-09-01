f3f013e7510141446ed40350b15a0d25b2c3b637
Alexey <Alexey.gt@bk.ru>
2023-09-01 14:17:44 +0300

    Homework_1

---
+++ Homework_1/homework_1.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def shop_base():  # put application's code here
+    return render_template('shop_base.html')
+
+
+@app.route('/clothes/')
+def clothes():  # put application's code here
+    return render_template('clothes.html')
+
+
+@app.route('/shoes/')
+def shoes():  # put application's code here
+    return render_template('shoes.html')
+
+
+@app.route('/clothes/jackets/')
+def jacket():  # put application's code here
+    return render_template('jackets.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from Lection_1.Lection_1 import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
